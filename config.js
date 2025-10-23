151736eddac8c0cc2860e6e84da8c733200c05d7
ii7modysmp-hue <ii7modysmp@gmail.com>
2025-10-23 09:51:38 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+// GitHub Configuration
+const GITHUB_CONFIG = {
+    username: 'ii7modysmp-hue',
+    repo: 'mod',
+    branch: 'main',
+    token: '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'
+};
+
+// مسارات الملفات في GitHub
+const PATHS = {
+    images: 'images/',
+    mods: 'mods/',
+    data: 'data/'
+};
+
+// رابط قاعدة GitHub
+const GITHUB_BASE_URL = `https://raw.githubusercontent.com/${GITHUB_CONFIG.username}/${GITHUB_CONFIG.repo}/${GITHUB_CONFIG.branch}/`;
